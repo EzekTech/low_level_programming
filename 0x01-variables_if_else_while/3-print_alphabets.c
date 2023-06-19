a88c09516bab6642d7bffab18010b1cc9924dd13
EzekTech <mofenice700@gmail.com>
2023-06-19 10:43:04 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alphabet = 'a';
+
+	char ALPHABET = 'A';
+
+	while (alphabet <= 'z')
+	{
+		putchar(alphabet);
+
+		alphabet++;
+	}
+
+
+	while (ALPHABET <= 'Z')
+	{
+		putchar(ALPHABET);
+
+		ALPHABET++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
